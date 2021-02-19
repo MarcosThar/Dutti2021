7d39443dcef6a563afa30a74ae11d2f02828a2c8
MarcosTh <49874194+MarcosThar@users.noreply.github.com>
2021-02-19 22:15:40 +0100

    Reubicacion de Componentes
    - Reubicación de los componentes de tipo de página en el directorio 'pages' para que sea mas descriptivo , ya que esos componentes se usan como paginas, (#1)
    - Anidación de los componentes secundarios de 'PrincipalComponent' dentro del directorio 'src/app/pages/principal', con la finalidad de ser mas descriptivo, ya que esos componentes solo se usan dentro del 'PrincipalComponent'
    
    Co-authored-by: unknown <marr_th_@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// Components
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+// pages
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
-  // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
+  { path: 'principal', loadChildren: () => import(`./pages/principal/principal.module`).then(m => m.PrincipalModule) }
+  // { path: 'ships', loadChildren: () => import(`./pages/ships/ships.module`).then(m => m.ShipsModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { PrincipalModule } from './components/principal/principal.module';
+import { PrincipalModule } from './pages/principal/principal.module';
 
-// Components
+// pages
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { PrincipalComponent } from './components/principal/principal.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
 
 
 @NgModule({
