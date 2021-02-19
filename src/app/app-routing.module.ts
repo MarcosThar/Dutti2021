3724356c803207a89a3dc1dc20ff95ffc4528689
MarcosTh <49874194+MarcosThar@users.noreply.github.com>
2021-02-19 22:22:24 +0100

    Feature/login register modules (#2)
    Se implementan modulos independientes con carga diferida (lazy loading) para las paginas login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// pages
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'principal', loadChildren: () => import(`./pages/principal/principal.module`).then(m => m.PrincipalModule) }
-  // { path: 'ships', loadChildren: () => import(`./pages/ships/ships.module`).then(m => m.ShipsModule) }
+  {
+    path: '',
+    loadChildren: () => import(`./pages/login/login.module`).then(m => m.LoginModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import(`./pages/register/register.module`).then(m => m.RegisterModule)
+  },
+  {
+    path: 'principal',
+    loadChildren: () => import(`./pages/principal/principal.module`).then(m => m.PrincipalModule)
+  }
 ];
 
 @NgModule({
